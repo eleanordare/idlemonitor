7240bfe94d9e18517f592140cac31d90e83f23a9
root <eleanor@redhat.com>
2016-11-01 17:11:55 -0400

    service loader

--- src/main/java/idleMonitor/idleMonitor/CheckStatus.java
+++ src/main/java/idleMonitor/idleMonitor/CheckStatus.java
@@ -7,6 +7,26 @@ import java.util.ServiceLoader;
 
 public class CheckStatus {
 		
+	
+	public static <T> T loadService(Class<T> service) {
+		
+		T result = null;
+		ServiceLoader<T> impl = ServiceLoader.load(service);
+		
+		for (T loadedImpl : impl) {
+			result = loadedImpl;
+			if (result != null) break;
+		}
+		
+		if (result == null) throw new RuntimeException(
+				"Cannot find implementation for: " + service);
+		
+		return result;
+		
+	}
+	
+	public static final Constraints constraints = loadService(Constraints.class);
+	
 	/*
 	 * compares timeout period specified in Setup with last time UI was hit
 	 * and checks if busy executors is 0, calls shutdown script if necessary
@@ -16,10 +36,7 @@ public class CheckStatus {
 		// check if latest hit is before time limit
 		if ((latest.isBefore(new DateTime(new DateTime().minus(period)))) && busyExecutors == 0) {
 			System.out.println("sleepy time");
-			ServiceLoader<Constraints> serviceLoader = ServiceLoader.load(Constraints.class);
-			for (Constraints constraints : serviceLoader) {
-				constraints.shutdownJenkins();
-			}
+			constraints.shutdownJenkins();
 			return false;
 		}
 		else {
