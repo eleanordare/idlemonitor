113ef9c7e7a8131e4f45c24b945cc2a32103a141
root <eleanor@redhat.com>
2016-11-02 13:29:58 -0400

    tests

---
+++ src/test/java/idleMonitor/CheckStatusTest.java
@@ -0,0 +1,47 @@
+package idleMonitor;
+
+import static org.junit.Assert.*;
+import idleMonitor.idleMonitor.CheckStatus;
+
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+import org.junit.Ignore;
+import org.junit.Test;
+
+@Ignore
+public class CheckStatusTest {
+	
+	CheckStatus check = new CheckStatus();
+	
+	@Test
+	public void testFalse() {
+		
+		long busyExecutors = 0;
+    	Period period = Period.days(7);
+    	DateTime latest = new DateTime(new DateTime().minusDays(10));
+    	
+    	assertFalse(check.main(busyExecutors, latest, period));
+		
+	}
+	
+	public void testTrue1() {
+		
+		long busyExecutors = 5;
+    	Period period = Period.days(7);
+    	DateTime latest = new DateTime(new DateTime().minusDays(10));
+    	
+    	assertTrue(check.main(busyExecutors, latest, period));
+		
+	}
+	
+	public void testTrue2() {
+		
+		long busyExecutors = 0;
+    	Period period = Period.days(7);
+    	DateTime latest = new DateTime(new DateTime().minusDays(5));
+    	
+    	assertTrue(check.main(busyExecutors, latest, period));
+    	
+	}
+
+}
